dad9cd4b2d8a7d3641339365c60fde00b58e48d7
LordReavan <cwinger000@citymail.cuny.edu>
2024-10-23 13:14:49 -0400

    Copied and complied game

---
+++ Assets/Scripts/ColorChangerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangerController : MonoBehaviour
+{
+    //This is about as simple a script as you can imagine
+    //It makes it so that if you hit the space bar, the attached sprite changes colors
+    
+    //This is the SpriteRenderer component in charge of drawing this object's sprite
+    public SpriteRenderer SR;
+    public Vector3 vel = new Vector3(0, 0, 0);
+
+    //This is the color we want the object to turn
+    public Color TargetColor = Color.red;
+    
+    //Any code inside of Update's {} brackets runs once per frame
+    void Update()
+    {
+        //This if statement can be read "If I hit space, change the sprite's color"
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Here we update the SpriteRenderer's color to be whatever TargetColor is set to be
+            SR.color = TargetColor;
+        }
+    }
+}
